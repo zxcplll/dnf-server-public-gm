e95069d0a2eb03f31f15652fc0c200a1b0b087fb
zxcplll <zxcplll@users.noreply.github.com>
2026-07-28 21:17:49 +0800

    feat: add midnight admin theme

--- webui2/src/main.ts
+++ webui2/src/main.ts
@@ -3,6 +3,8 @@ import ArcoVue from '@arco-design/web-vue';
 import App from './App.vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import '@arco-design/web-vue/dist/arco.css';
+import './style.css';
+import './styles/admin-theme.css';
 import router from './router';
 
 const app = createApp(App);
